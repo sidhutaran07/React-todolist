215c33f0f1afaa03835d381169ed58aa65f3f067
sidhutaran07 <taranpreets194@gmail.com>
2025-08-31 01:52:56 +0530

    Update firebaseAdmin.js

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,10 +1,21 @@
 const admin = require("firebase-admin");
 
-// Parse the service account key JSON stored in Render env variable
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+let serviceAccount;
+try {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+  if (serviceAccount.private_key) {
+    // Fix escaped newlines
+    serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+  }
+} catch (err) {
+  console.error("Failed to parse FIREBASE_SERVICE_ACCOUNT_KEY:", err);
+  throw new Error("Invalid service account key in environment variables");
+}
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 module.exports = admin;
